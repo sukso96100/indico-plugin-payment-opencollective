c08caa122b54aef273bb48e552f46296c4b67678
Youngbin Han <ybhan@ubuntu.com>
2024-10-08 08:55:41 +0000

    Use OpenCollective v2 GraphQL API to get order data in callback handler

--- indico_payment_opencollective/controllers.py
+++ indico_payment_opencollective/controllers.py
@@ -11,6 +11,8 @@ import requests
 from flask import flash, redirect, request
 from flask_pluginengine import current_plugin
 from werkzeug.exceptions import BadRequest
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
 
 from indico.modules.events.payment.models.transactions import TransactionAction
 from indico.modules.events.payment.notifications import notify_amount_inconsistency
@@ -23,7 +25,244 @@ from indico_payment_opencollective import _
 
 
 IPN_VERIFY_EXTRA_PARAMS = (('cmd', '_notify-validate'),)
-OC_API_BASEURL = "https://api.opencollective.com"
+OC_API_BASEURL = "https://api.opencollective.com/graphql/v2"
+OC_GQL_ORDER_QUERY = """
+query (
+  $order: OrderReferenceInput!
+) {
+  order(order: $order) {
+    id
+    legacyId
+    description
+    amount {
+      value
+      currency
+      valueInCents
+    }
+    taxAmount {
+      value
+      currency
+      valueInCents
+    }
+    totalAmount {
+      value
+      currency
+      valueInCents
+    }
+    quantity
+    status
+    frequency
+    nextChargeDate
+    tier {
+      id
+      legacyId
+      slug
+      name
+      description
+      longDescription
+      videoUrl
+      currency
+      button
+      type
+      frequency
+      presets
+      maxQuantity
+      availableQuantity
+      customFields
+      amountType
+      endsAt
+      invoiceTemplate
+      useStandalonePage
+      singleTicket
+      requireAddress
+    }
+    fromAccount {
+      id
+      slug
+      type
+      name
+      legalName
+      description
+      longDescription
+      tags
+      currency
+      expensePolicy
+      isIncognito
+      imageUrl
+      backgroundImageUrl
+      createdAt
+      updatedAt
+      isArchived
+      isFrozen
+      isActive
+      isHost
+      isAdmin
+      emails
+      settings
+      supportedExpenseTypes
+      categories
+    }
+    toAccount {
+      id
+      slug
+      type
+      name
+      legalName
+      description
+      longDescription
+      tags
+      currency
+      expensePolicy
+      isIncognito
+      imageUrl
+      backgroundImageUrl
+      createdAt
+      updatedAt
+      isArchived
+      isFrozen
+      isActive
+      isHost
+      isAdmin
+      emails
+      settings
+      supportedExpenseTypes
+      categories
+    }
+    transactions {
+      id
+      legacyId
+      group
+      type
+      kind
+      description
+      hostCurrencyFxRate
+      createdAt
+      updatedAt
+      isRefunded
+      isRefund
+      isDisputed
+      isInReview
+      isOrderRejected
+      merchantId
+      invoiceTemplate
+    }
+    createdAt
+    updatedAt
+    totalDonations {
+      value
+      currency
+      valueInCents
+    }
+    paymentMethod {
+      id
+      legacyId
+      name
+      service
+      type
+      data
+      expiryDate
+      createdAt
+    }
+    hostFeePercent
+    platformTipAmount {
+      value
+      currency
+      valueInCents
+    }
+    platformTipEligible
+    tags
+    tax {
+      id
+      type
+      rate
+      idNumber
+    }
+    tax {
+      type
+      rate
+    }
+    membership {
+      id
+      role
+      createdAt
+      updatedAt
+      since
+      publicMessage
+      description
+      inherited
+    }
+    permissions {
+      id
+      canMarkAsExpired
+      canMarkAsPaid
+      canEdit
+      canComment
+      canSeePrivateActivities
+      canSetTags
+      canUpdateAccountingCategory
+    }
+    accountingCategory {
+      id
+      code
+      name
+      friendlyName
+      hostOnly
+      instructions
+      expensesTypes
+      kind
+      createdAt
+    }
+    activities {
+      offset
+      limit
+      totalCount
+    }
+    data
+    customData
+    memo
+    createdByAccount {
+      id
+      slug
+      type
+      name
+      legalName
+      description
+      longDescription
+      tags
+      currency
+      expensePolicy
+      isIncognito
+      imageUrl
+      backgroundImageUrl
+      createdAt
+      updatedAt
+      isArchived
+      isFrozen
+      isActive
+      isHost
+      isAdmin
+      emails
+      settings
+      supportedExpenseTypes
+      categories
+    }
+    processedAt
+    pendingContributionData {
+      expectedAt
+      paymentMethod
+      ponumber
+      memo
+    }
+    needsConfirmation
+    comments{
+      offset
+      limit
+      totalCount
+    }
+  }
+}
+
+"""
 
 oc_tx_order_status_action_mapping = {'PAID': TransactionAction.complete,
                                      'REJECTED': TransactionAction.reject,
@@ -43,37 +282,56 @@ class RHOpenCollectivePostPaymentCallback(RH):
             raise BadRequest
 
     def _process(self):
-        oc_transactionid = request.args.get('transactionid') 
+        oc_order_id = request.args.get('orderId') 
+        oc_order_id_v2 = request.args.get('orderIdV2') 
+        oc_status = request.args.get('status')
         slug = current_plugin.settings.get('event_slug')
         if not slug:
             slug = current_plugin.settings.get('collective_slug')
+
+
         oc_tx_response = requests.get(f"{OC_API_BASEURL}/v1/collectives/{slug}/transactions/{oc_transactionid}")
-        oc_tx_result = oc_tx_response.json()
-        oc_tx_amount = oc_tx_result['result']['amount'] / 100
-        oc_tx_currency = oc_tx_result['result']['currency']
-        oc_tx_order_status = oc_tx_result['result']['order']['status']
-        oc_tx_payee_slug = oc_tx_result['result']['collective']['slug']
+
+        transport = RequestsHTTPTransport(
+            url=OC_API_BASEURL,
+            verify=True,
+            retries=3,
+        )
+
+        client = Client(transport=transport, fetch_schema_from_transport=True)
+        gql_query = gql(OC_GQL_ORDER_QUERY)
+        gql_params = {
+            "order": {
+                "id": oc_order_id_v2,
+                "legacyId": int(oc_order_id)
+                }
+            }
+        oc_order_result = client.execute(gql_query, variable_values=gql_params)
+        oc_order_amount = oc_order_result['data']['order']['amount']['value']
+        oc_order_currency = oc_order_result['data']['order']['amount']['currency']
+        oc_order_order_status = oc_order_result['data']['order']['status']
+        oc_order_payee_slug = oc_order_result['data']['order']['toAccount']['slug']
 
         # Check if amount paid to correct collective
-        if slug != oc_tx_payee_slug:
-            current_plugin.logger.warning(f"Payment made to wrong collective (Expected: {slug}, Actual: {oc_tx_payee_slug})")
+        if slug != oc_order_payee_slug:
+            current_plugin.logger.warning(f"Payment made to wrong collective (Expected: {slug}, Actual: {oc_order_payee_slug})")
             return
-        self._verify_amount(oc_tx_result)
+        self._verify_amount(oc_order_result)
         register_transaction(registration=self.registration,
-                             amount=float(oc_tx_amount),
-                             currency=oc_tx_currency,
-                             action=oc_tx_order_status_action_mapping[oc_tx_order_status],
+                             amount=float(oc_order_amount),
+                             currency=oc_order_currency,
+                             action=oc_tx_order_status_action_mapping[oc_order_order_status],
                              provider='opencollective',
-                             data=oc_tx_result)
+                             data=oc_order_result)
         flash(_('Your payment request has been processed.'), 'success')
         return redirect(url_for('event_registration.display_regform', self.registration.locator.registrant))
 
 
-    def _verify_amount(self, oc_tx_result: dict):
+    def _verify_amount(self, oc_order_result: dict):
         expected_amount = self.registration.price
         expected_currency = self.registration.currency
-        amount = oc_tx_result['result']['amount'] / 100
-        currency = oc_tx_result['result']['currency']
+        amount = oc_order_result['data']['order']['amount']['value']
+        currency = oc_order_result['data']['order']['amount']['currency']
         if expected_amount == amount and expected_currency == currency:
             return True
         current_plugin.logger.warning("Payment doesn't match event's fee: %s %s != %s %s",
