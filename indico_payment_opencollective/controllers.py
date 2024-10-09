ab7c94c5e99870bf9acf2481a429de7ae5522820
Youngbin Han <ybhan@ubuntu.com>
2024-10-09 03:38:11 +0000

    Show error for invlaid payment

--- indico_payment_opencollective/controllers.py
+++ indico_payment_opencollective/controllers.py
@@ -67,26 +67,43 @@ class RHOpenCollectivePostPaymentCallback(RH):
         oc_order_payee_slug = oc_order_result['order']['toAccount']['slug']
         oc_order_payment_frequency = oc_order_result['order']['frequency']
 
-        # Check if amount paid to correct collective
+        is_payment_valid = True
+        invliad_error_msgs = []
+        
         if slug != oc_order_payee_slug:
-            current_plugin.logger.warning(f"Payment made to wrong collective (Expected: {slug}, Actual: {oc_order_payee_slug})")
-            return
+            error_msg = f"Payment made to wrong collective (Expected: {slug}, Actual: {oc_order_payee_slug})"
+            current_plugin.logger.warning(error_msg)
+            invliad_error_msgs.append(error_msg)
+            is_payment_valid = False
         if oc_order_payment_frequency != "ONETIME":
-            current_plugin.logger.warning(f"Payment must be ONETIME (Expected: ONETIME, Actual: {oc_order_payment_frequency})")
-            return
+            error_msg = f"Payment must be ONETIME (Expected: ONETIME, Actual: {oc_order_payment_frequency})"
+            current_plugin.logger.warning(error_msg)
+            invliad_error_msgs.append(error_msg)
+            is_payment_valid = False
         if self.oc_order_status != oc_order_order_status:
             current_plugin.logger.warning(
                 f"Payment status from callback url and graphql response not matches. \
                     This could be due to update on order after callback data received from url \
                         (From URL: {self.oc_order_status}, From GraphQL Response: {oc_order_order_status})")
-        self._verify_amount(oc_order_result)
-        register_transaction(registration=self.registration,
-                             amount=float(oc_order_amount),
-                             currency=oc_order_currency,
-                             action=oc_tx_order_status_action_mapping[oc_order_order_status],
-                             provider='opencollective',
-                             data=oc_order_result)
-        flash(_('Your payment request has been processed.'), 'success')
+        if is_payment_valid:
+            is_amount_valid = self._verify_amount(oc_order_result)
+            register_transaction(registration=self.registration,
+                                amount=float(oc_order_amount),
+                                currency=oc_order_currency,
+                                action=oc_tx_order_status_action_mapping[oc_order_order_status],
+                                provider='opencollective',
+                                data=oc_order_result)
+            if is_amount_valid:
+                flash(_('Your payment request has been processed.'), 'success')
+            else:
+                flash(_('Your payment request has been processed. But paid amount not matches. \
+                        Please contact organizers to check and resolve this issue.'), 'warning')
+        else:
+            warning_msg = 'Your payment request is not valid. Thus, not recorded for this registration. \
+                Please contact organizers to check and resolve this issue.'
+            for msg_item in invliad_error_msgs:
+                warning_msg = f"{warning_msg}<br/>{msg_item}"
+            flash(_(warning_msg), 'error')
         return redirect(url_for('event_registration.display_regform', self.registration.locator.registrant))
 
 
